d481e741cf5f7943a61a5fbbb2d6d7bc5e517eb8
shivubu <155102583+shivubu@users.noreply.github.com>
2025-04-17 01:11:13 +0530

    Update:Added Zero-Three and all its associated sound effects

--- app/src/main/java/com/example/kamen_app_01/MainActivity.java
+++ app/src/main/java/com/example/kamen_app_01/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     MediaPlayer mp,end;
     private Drawable[] backgroundImages;
-    private int currentImageIndex = 0,flag=0;
+    private int currentImageIndex = 0,flag=0,zt_index,ztweap_index,zt_flag=0;
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +54,13 @@ public class MainActivity extends AppCompatActivity {
                 AppCompatResources.getDrawable(this,R.drawable.crossbuild),
                 AppCompatResources.getDrawable(this,R.drawable.evoltblackhole),
                 AppCompatResources.getDrawable(this,R.drawable.omaz),
+                AppCompatResources.getDrawable(this,R.drawable.zerothree),
                 AppCompatResources.getDrawable(this,R.drawable.geatsdea)};
-        int[] sounds = {R.raw.seraph0,R.raw.faiznext,R.raw.decadecomplete21,R.raw.oootajadoreternity,R.raw.exaidnovel,R.raw.genmmusou,R.raw.crossbuild,R.raw.evolblackhole,R.raw.omazio,R.raw.geatsdea};
-        int[] henshinsounds={R.raw.henshinseraph,R.raw.henshinfaiznext,R.raw.henshindecadecomplete21,R.raw.henshinoootajadoreternity,R.raw.henshinexaidnovel,R.raw.henshingenmmusou,R.raw.henshincrossbuild,R.raw.henshinevolblackhole,R.raw.henshinzioohma,R.raw.henshingeatsdea2};
-        int[] longpresssounds={R.raw.lpseraph,R.raw.lpfaiznext,R.raw.lpdecadecomplete21,R.raw.lpoootajadoreternity,R.raw.lpexaidnovel,R.raw.lpgenmmusou,R.raw.lpcrossbuild,R.raw.lpevolblackhole,R.raw.lpohma,R.raw.lpgeatsdea};
+        int[] sounds = {R.raw.seraph0,R.raw.faiznext,R.raw.decadecomplete21,R.raw.oootajadoreternity,R.raw.exaidnovel,R.raw.genmmusou,R.raw.crossbuild,R.raw.evolblackhole,R.raw.omazio,R.raw.zerothree,R.raw.geatsdea};
+        int[] henshinsounds={R.raw.henshinseraph,R.raw.henshinfaiznext,R.raw.henshindecadecomplete21,R.raw.henshinoootajadoreternity,R.raw.henshinexaidnovel,R.raw.henshingenmmusou,R.raw.henshincrossbuild,R.raw.henshinevolblackhole,R.raw.henshinzioohma,R.raw.henshinzerothree,R.raw.henshingeatsdea2};
+        int[] longpresssounds={R.raw.lpseraph,R.raw.lpfaiznext,R.raw.lpdecadecomplete21,R.raw.lpoootajadoreternity,R.raw.lpexaidnovel,R.raw.lpgenmmusou,R.raw.lpcrossbuild,R.raw.lpevolblackhole,R.raw.lpohma,R.raw.lpzerothree,R.raw.lpgeatsdea};
+        int[] zt_sounds={R.raw.zt_create,R.raw.zt_singularity,R.raw.zt_ability,R.raw.zt_there_ark_ability,R.raw.zt_outsiders_ability};
+        int[] ztweap_sounds={R.raw.attache_calibur,R.raw.attache_shotgun,R.raw.attache_arrow,R.raw.shotriser,R.raw.slashriser,R.raw.thousand_jacker,R.raw.authorise_blaster,R.raw.hopper_blade};
         ArrayList<Integer> sound = new ArrayList<>();
         for (int j : sounds) {
             sound.add(j);
@@ -69,6 +72,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Integer> longpress = new ArrayList<>();
         for (int longpresssound : longpresssounds) {
             longpress.add(longpresssound);
+        }
+        ArrayList<Integer> zt_sound = new ArrayList<>();
+        for (int j : zt_sounds) {
+            zt_sound.add(j);
+        }
+        ArrayList<Integer> ztweap_sound = new ArrayList<>();
+        for (int j : ztweap_sounds) {
+            ztweap_sound.add(j);
         }
         imageView = findViewById(R.id.imageView);
         imageView.setFocusable(true);
@@ -114,6 +125,11 @@ public class MainActivity extends AppCompatActivity {
                 // Update the background image
                 if (backgroundImages.length > 0) {
                     imageView.setImageDrawable(backgroundImages[currentImageIndex]);
+                    if(currentImageIndex==9)
+                    {
+                        zt_index=-1;
+                        ztweap_index=-1;
+                    }
                 }
                 return true;
             }
@@ -134,18 +150,25 @@ public class MainActivity extends AppCompatActivity {
                     {
                         mp=MediaPlayer.create(MainActivity.this,R.raw.lpgenmmusoualt);
                         mp.start();
-                        mp.setOnCompletionListener(mp -> {
-                            imageView.clearAnimation();
-                            flag=2;
-                        });
+                        flag=2;
+                        mp.setOnCompletionListener(mp -> imageView.clearAnimation());
+                    }
+                    else if(currentImageIndex==9 && zt_flag==1)
+                    {
+                        mp = MediaPlayer.create(MainActivity.this, R.raw.lpzerothreealt);
+                        mp.start();
+                        zt_flag=0;
+                        mp.setOnCompletionListener(mp -> imageView.clearAnimation());
                     }
                     else
                     {
                         mp = MediaPlayer.create(MainActivity.this, longpress.get(currentImageIndex));
                         mp.start();
-                        mp.setOnCompletionListener(mp -> {
-                            imageView.clearAnimation();
-                        });
+                        if(currentImageIndex==9 && zt_flag==0)
+                        {
+                            zt_flag=1;
+                        }
+                        mp.setOnCompletionListener(mp -> imageView.clearAnimation());
 
                     }
                     super.onLongPress(e);
@@ -179,6 +202,11 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+                    assert e1 != null;
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    float SWIPE_THRESHOLD_VELOCITY = 200;
+                    float SWIPE_THRESHOLD_DISTANCE = 100;
                     if(mp!=null)
                     {
                         mp.release();
@@ -186,37 +214,80 @@ public class MainActivity extends AppCompatActivity {
                     }
                     if(currentImageIndex==5)
                     {
-                        if(flag==0)
+                        if(diffX < -SWIPE_THRESHOLD_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY && Math.abs(diffY) < SWIPE_THRESHOLD_DISTANCE && flag==0)
                         {
                             imageView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.customfade));
                             mp=MediaPlayer.create(MainActivity.this,R.raw.genmpause);
                             mp.start();
-                            mp.setOnCompletionListener(mp -> {
-                                imageView.clearAnimation();
-                                flag=1;
-                            });
+                            flag=1;
+                            mp.setOnCompletionListener(mp -> imageView.clearAnimation());
                         }
-                        if(flag==1)
+                        if(diffX > SWIPE_THRESHOLD_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY && Math.abs(diffY) < SWIPE_THRESHOLD_DISTANCE)
                         {
-                            imageView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.customfade));
-                            mp=MediaPlayer.create(MainActivity.this,R.raw.genmrestart);
-                            mp.start();
-                            mp.setOnCompletionListener(mp -> {
-                                imageView.clearAnimation();
+                            if(flag==1)
+                            {
+                                imageView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.customfade));
+                                mp=MediaPlayer.create(MainActivity.this,R.raw.genmrestart);
+                                mp.start();
                                 flag=0;
-                            });
-                        }
-                        if(flag==2)
-                        {
-                            imageView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.customfade));
-                            mp=MediaPlayer.create(MainActivity.this,R.raw.genmrestartalt);
-                            mp.start();
-                            mp.setOnCompletionListener(mp -> {
-                                imageView.clearAnimation();
+                                mp.setOnCompletionListener(mp -> imageView.clearAnimation());
+                            }
+                            if(flag==2)
+                            {
+                                imageView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.customfade));
+                                mp=MediaPlayer.create(MainActivity.this,R.raw.genmrestartalt);
+                                mp.start();
                                 flag=0;
-                            });
+                                mp.setOnCompletionListener(mp -> imageView.clearAnimation());
+                            }
+
                         }
 
+                    }
+                    if(currentImageIndex==9)
+                    {
+                        if(diffX > SWIPE_THRESHOLD_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY && Math.abs(diffY) < SWIPE_THRESHOLD_DISTANCE)
+                        {
+                            zt_index++;
+                            if(zt_index>=zt_sounds.length)
+                            {
+                                zt_index=0;
+                            }
+                            mp=MediaPlayer.create(MainActivity.this,zt_sound.get(zt_index));
+                            mp.start();
+                        }
+                        if(diffX < -SWIPE_THRESHOLD_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY && Math.abs(diffY) < SWIPE_THRESHOLD_DISTANCE)
+                        {
+                            zt_index--;
+                            if(zt_index<0)
+                            {
+                                zt_index=zt_sounds.length-1;
+                            }
+                            mp=MediaPlayer.create(MainActivity.this,zt_sound.get(zt_index));
+                            mp.start();
+                        }
+                        if(diffY < -SWIPE_THRESHOLD_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY && Math.abs(diffX) < SWIPE_THRESHOLD_DISTANCE)
+                        {
+                            ztweap_index++;
+                            if(ztweap_index>=ztweap_sounds.length)
+                            {
+                                ztweap_index=0;
+                            }
+                            mp=MediaPlayer.create(MainActivity.this,ztweap_sound.get(ztweap_index));
+                            mp.start();
+
+                        }
+                        if(diffY > SWIPE_THRESHOLD_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY && Math.abs(diffX) < SWIPE_THRESHOLD_DISTANCE)
+                        {
+                            ztweap_index--;
+                            if(ztweap_index<0)
+                            {
+                                ztweap_index=ztweap_sounds.length-1;
+                            }
+                            mp=MediaPlayer.create(MainActivity.this,ztweap_sound.get(ztweap_index));
+                            mp.start();
+
+                        }
                     }
                     return super.onFling(e1, e2, velocityX, velocityY);
                 }
